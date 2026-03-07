ae6b55bccc9a171e2173311e53afdecb61792ed7
Anh Duy <duy796017@gmail.com>
2026-03-07 23:15:19 +0700

    remove assignment 4 files

--- Assignment 4/Ex4.py
+++
@@ -1,9 +0,0 @@
-PN = input("Enter your phone number:")
-def phone_number(PN):
-    if len(PN) == 10 and PN.isdigit():
-        return "[REDACTED]"
-    if PN.startswith("+84") and PN[1:].isdigit() and len(PN) == 12:
-        return "[REDACTED]"
-    return PN
-
-print(phone_number(PN))
