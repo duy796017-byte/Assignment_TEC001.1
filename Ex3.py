6f31f970073a6bbd3d2350e882e4f4dfe64e95da
Anh Duy <duy796017@gmail.com>
2026-03-07 23:30:20 +0700

    add assignment 6

---
+++ Ex3.py
@@ -0,0 +1,12 @@
+names = set()
+while True:
+    name = input("Enter name: ")
+    if name == "":
+        break
+    if name in names:
+        print("Existing name")
+    else:
+        print("New name")
+    names.add(name)
+for n in names:
+    print(n)
